4d5c1e549e45f1bfc74cd114661ef3e2d76f5aed
VycheslavN <masterdog8@mail.ru>
2020-08-11 22:40:43 +0300

    Initial commit

---
+++ src/Object.java
@@ -0,0 +1,12 @@
+public class Object {
+
+    public class Weapon { //оружие
+        public void main(String[] args) {
+        }
+    }
+
+    public class Protection { //защита
+        public void main(String[] args) {
+        }
+    }
+}
